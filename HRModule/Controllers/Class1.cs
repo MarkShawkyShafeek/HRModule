1ac407bb589f67f66d3cd1b18857aba25cbc0441
Mark Shawky <mark.shawky.shafeek@gmail.com>
2019-05-21 13:29:36 +0200

    first commit

---
+++ HRModule/Controllers/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HRModule;
+using MyGeneration.dOOdads;
+using HRModuleBL;
+
+namespace HRModule.Controllers
+{
+    public class Class1
+    {
+        Attatchment tt = new Attatchment();
+        MaritalStatus sds = new MaritalStatus();
+        void test()
+        {
+            
+        }
+    }
+}
